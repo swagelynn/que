3c7e91f80e2a350d9b2801163c7d20c1fce42b62
s4midev <astingraye@proton.me>
2025-12-08 01:35:03 +0000

    remove overbearing title restrictions and fix webhooks (hopefully)

--- notify.go
+++ notify.go
@@ -11,9 +11,10 @@ func notify(q Question) {
 	webhook := os.Getenv("DISCORD_WEBHOOK")
 
 	if webhook != "" {
+		fmt.Println(webhook)
 		client := hook.NewClient(webhook)
 
-		embed := hook.NewEmbed().SetTitle(q.Title).SetDescription(q.Body).SetAuthor(q.Author, "", "")
+		embed := hook.NewEmbed().SetTitle(q.Title).SetDescription(q.Body).SetAuthor(q.Author, "https://duckduckgo.com/?t=ffab&q=%22"+q.Author+"%22", "https://files.catbox.moe/7hz92d.jpeg")
 
 		if err := client.SendEmbed(embed); err != nil {
 			fmt.Println("Invalid webhook url supplied")
